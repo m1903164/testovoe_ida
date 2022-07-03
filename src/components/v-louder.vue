ab8e719218e5106b421e2dc93b14054c3d267ee8
vgrishin <87881773+m1903164@users.noreply.github.com>
2022-07-03 21:04:48 +0300

    [ lin-4 ]

---
+++ src/components/v-louder.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="loader">
+    <div class="lds-ripple"><div></div><div></div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VLoder'
+}
+</script>
+
+<style lang="scss" scoped>
+.loader {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .lds-ripple {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+    div {
+      position: absolute;
+      border: 4px solid #3F3F3F;
+      opacity: 1;
+      border-radius: 50%;
+      animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+      &:nth-child(2) {
+        animation-delay: -0.5s;
+      }
+    }
+  }
+  @keyframes lds-ripple {
+    0% {
+      top: 36px;
+      left: 36px;
+      width: 0;
+      height: 0;
+      opacity: 1;
+    }
+    100% {
+      top: 0;
+      left: 0;
+      width: 72px;
+      height: 72px;
+      opacity: 0;
+    }
+  }
+}
+</style>
--- src/components/v-productlist.vue
+++ src/components/v-productlist.vue
@@ -1,25 +1,42 @@
 <template>
     <div class="prod-list">
-        <VProduct
-            v-for="product in products"      
-            v-bind:key="product.id"
-            :product="product"
-            @remove="$emit('remove', product)"
-        />
+        <transition-group name="fade">
+            <VLouder
+                v-if="isLoading"
+            />
+            <VProduct
+                v-else v-for="product in products"      
+                v-bind:key="product.id"
+                :product="product"
+                @remove="$emit('remove', product)"
+            />
+        </transition-group>
     </div>
 </template>
 
 <script>
 import VProduct from "./v-product.vue";
+import VLouder from "./v-louder.vue";
 export default {
     name: "VproductList",
+    components: { VProduct, VLouder },
+    data() {
+        return {
+            isLoading: true
+        }
+    },
     props: {
         products: {
             type: Array,
             required: true,
         }
     },
-    components: { VProduct }
+
+    created() {
+        setTimeout(() => {
+            this.isLoading = false
+        },1200)
+    }
 }
 </script>
 
@@ -30,5 +47,16 @@ export default {
         gap: 16px;
         position: relative;
         min-height: 100px;
+    }
+    .fade-enter-active,
+    .fade-leave-active {
+     transition: opacity 0.5s ease;
+    }
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+    }
+     .fade-move {
+        transition: transform 0.5s ease;
     }
 </style>
